dd8fbad8309cc679f10ab6a7b9fcba8e0c4c10e0
shy <shenghouyuan@163.com>
2018-10-19 09:40:19 +0800

    test

--- template/src/main/scala/bhfae/Common.scala
+++ template/src/main/scala/bhfae/Common.scala
@@ -1,12 +1,31 @@
 package bhfae
 
-import utils.DateTool
-
 
 object Common {
 
 	def main(args: Array[String]) {
-		println(DateTool.getNowDate)
+
+		val v2=2
+
+		val res=v2 match {
+			case 1=> 1
+			case 2=> 2
+			case _=> 3
+		}
+
+		println(res)
+
+		def  f(str:String)="f("+str+")"
+		def  g(str:String)="g("+str+")"
+
+		val fg=f _ compose g
+		println(fg("success"))
+
+
 	}
+
+
+
+
 
 }
